1853abfb510cd21f301e481a542929002c6a013a
+dennisngangairungu <+>
2023-10-07 15:56:47 +0300

    added comments

--- Net-Salary-Calculator.js
+++ Net-Salary-Calculator.js
@@ -1,10 +1,14 @@
+// the prompt below is used to import 'prompt-sync' to enable  entering of salary
 const prompt = require('prompt-sync')();
+// this const variable is used to store the 'basicSalary' to be used as a argument
 const basicSalary=prompt('Enter your basic salary :');
 
 
 
 
 function netSalaryCaculator(basicSalary){
-    
+
 }
+
+//invocing the netSalaryCaculator() to return
 netSalaryCaculator(basicSalary)
